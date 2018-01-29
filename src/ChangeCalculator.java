83974e1d24ed35cf5937f7f425c0d36b4d647e3b
Student_2 <Student_2@165.24.36.176>
2018-01-29 14:22:19 -0800

    change calulator fruit quiz Pi aloud

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,17 +14,29 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
-		// Convert their answer to an int using Integer.parseInt()
-
+String N=JOptionPane.showInputDialog(null,"How many nickels do you have");
+		// Convert their answer to an "int" using Integer.parseInt()
+int money=Integer.parseInt(N);
 		// Ask the user how many dimes they have, and convert their answer
+String D=JOptionPane.showInputDialog("How many Dimes do you have");
+int coins=Integer.parseInt(D);		
+// Ask the user how many quarters they have, and convert their answer
+		String Q=JOptionPane.showInputDialog("How Many Quarters do you have");
+		int quarts=Integer.parseInt(Q);
 
-		// Ask the user how many quarters they have, and convert their answer
-
-		// Calculate how much money the user has and save it in a double variable 
-
-		// Tell the user how much money they have
-
+		// Calculate how much money the user has and save it in a double variable
+		//0.05 * 5 = 0.25
+		double totaln= money*0.05;
+		double Totalm= coins*0.10;
+		double totalq= quarts*0.25;
+		double totalmoney= totaln+Totalm+totalq;
+		
+// Tell the user how much money they have
+JOptionPane.showMessageDialog(null,"you money is " +totalmoney1);
+	
+	
+	
 	}
+
 }
 
--- src/FruitQuiz.java
+++ src/FruitQuiz.java
@@ -23,16 +23,23 @@ public class FruitQuiz extends KeyAdapter {
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-		
+		System.out.println(keyCode);
 
 		// 2. Make 3 variables that hold the key codes for apple, banana and carrot
-
+int A= 65;
+int B= 66;
+int C= 67;
 		
 
 		
 		if (question1.isShowing()) {
 			// 3. If they touched the right fruit, do steps 4 and 7
-			
+		  if(keyCode==A){
+			  correct();
+			  
+			  
+		  }
+		  
 				// 4. Call the correct() method
 				
 				// 7. Use the nextQuestion() method to go to question2
@@ -51,6 +58,10 @@ public class FruitQuiz extends KeyAdapter {
 	}
 
 	private void correct() {
+		playSound("correct.wav");
+		
+		
+		
 		// 5. Find a sound for when they get a question right, and drag it into
 		// the default package. It must be a .wav file. 
 		// There are lots on freesound.org
